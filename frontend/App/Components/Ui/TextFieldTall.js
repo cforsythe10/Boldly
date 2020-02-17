efd55f72743ffce079064a2f5f52d8163630a4d5
cforsythe555 <40395596+cforsythe555@users.noreply.github.com>
2020-02-17 16:34:07 -0500

    Added new text fields and updated importants to dark background text input

---
+++ frontend/App/Components/Ui/TextFieldTall.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { View, TextInput } from 'react-native';
+import styles from './Styles/TextFieldTallStyles';
+
+export default class TextFieldTall extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: '',
+      style: {
+        ...styles.TextInput
+      }
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.BorderStyle}>
+        <TextInput
+          secureTextEntry={( this.state.text <= 0 && this.state.emailStatus!='onFocus' ) ? true : false }
+              placeholder='Insert Placeholder'
+              {...this.props}
+              style={this.state.style}
+              placeholderTextColor= '#C1C1C1'
+              theme={{ fonts: { regular: 'AvenirNext-Medium' } }}
+              onChangeText={(text) => {
+                if(text === '') {
+                  this.setState({
+                    text: text,
+                    style: {
+                      ...styles.TextInput,
+                      opacity: 0.5,
+                    }
+                }); 
+                } else {
+                  this.setState({
+                    text: text,
+                    style: {
+                      ...styles.TextInput,
+                      opacity: 1.0
+                    }
+                });
+              }
+            }}
+            />
+          </View>
+    )
+  }
+}
