aadcff68e4c43d51ff983a1d06b74c6a9ff0262e
Nataline <natie.kolbe@gmail.com>
2020-02-17 20:13:21 -0500

    working component

--- frontend/App/Components/Ui/SurveyProgressBar.js
+++ frontend/App/Components/Ui/SurveyProgressBar.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import ProgressBar from 'react-native-progress/Bar'
+import { View } from 'react-native'
+import ProgressBar from 'react-native-progress/Bar';
 import { Colors } from '../../Themes/'
 
 export default class SurveyProgressBar extends Component {
-  state = {
-    progress: 0
+  props = {
+    progress: PropTypes.number // this must be a decimal value between 0 and 1
   }
 
   render () {
     return (
-      <ProgressBar progress={0.5} width={100} height={30} 
-      color=Colors.white unfilledColor=Colors.fog borderWidth={0} />
+      <View>
+      <ProgressBar progress={this.props.progress} width={328} height={5} 
+      color={Colors.white} unfilledColor={Colors.fog} borderWidth={0} />
+      </View>
     )
   }
 }
