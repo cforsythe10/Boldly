2c19715a8a34b9b15e28fe5f4a7e888ec87cb1a6
Samuel Platek <moneymop@Samuels-MacBook-Pro.local>
2020-03-02 12:53:42 -0500

    Renamed makefile for ease of use
    Signed-off-by: Samuel Platek <moneymop@Samuels-MacBook-Pro.local>

--- backend/elixir/boldly/MAKE.mk
+++
@@ -1,6 +0,0 @@
-.PHONY=all
-
-
-
-setup-creators:
-	mix phx.gen.context CreatorAccount Creator creators uuid:uuid:unique name:string birthday:date selectedvalues:string industry:string interests:string location:string email:string:unique 
