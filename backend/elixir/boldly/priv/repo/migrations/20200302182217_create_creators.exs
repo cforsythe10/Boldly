7d9a31d5168e0ffb2bcccf87d98198648c08b01e
KedielM <km3556@drexel.edu>
2020-03-03 21:17:36 -0500

    Changes to be committed: 	modified:   priv/repo/migrations/20200302182217_create_creators.exs 	modified:   priv/repo/migrations/20200302220541_create_brands.exs  Fixed missing table values for creators and brands

--- backend/elixir/boldly/priv/repo/migrations/20200302182217_create_creators.exs
+++ backend/elixir/boldly/priv/repo/migrations/20200302182217_create_creators.exs
@@ -2,21 +2,25 @@ defmodule Boldly.Repo.Migrations.CreateCreators do
   use Ecto.Migration
 
   def change do
-    create table(:creators, primary_key: false) do
-      add :id, :uuid, primary_key: true, null: false
+    create table(:creators) do
+      add :uuid, :uuid, null: false
       add :name, :string, null: false
       add :birthday, :date, null: false
       add :values, :string
       add :industry, :string
       add :interests, :string
-      add :location, :string
+      add :location, :string, null: false
       add :email, :string, null: false
-      add :password_hash, :string
+      add :password_hash, :string, null: false
+      add :profile_picture, # what value for profile pic?
+      add :bio, :string, null: false
+      add :website, :url, null: false
+      add :featured_posts, # what value for gram posts?
 
       timestamps(type: :utc_datetime_usec)
     end
 
-    create unique_index(:creators, [:id])
+    create unique_index(:creators, [:uuid])
     create unique_index(:creators, [:email])
   end
 end
--- backend/elixir/boldly/priv/repo/migrations/20200302220541_create_brands.exs
+++ backend/elixir/boldly/priv/repo/migrations/20200302220541_create_brands.exs
@@ -5,12 +5,16 @@ defmodule Boldly.Repo.Migrations.CreateBrands do
     create table(:brands) do
       add :uuid, :uuid, null: false
       add :ecommerce, :boolean
-      add :location, :string,  null: false
-      add :industries, :string
+      add :location, :string, null: false
+      add :industry, :string
       add :values, :string
       add :email, :string, null: false
       add :password_hash, :string, null: false
       add :name, :string, null: false
+      add :profile_picture, # what value for profile pic?
+      add :bio, :string, null: false
+      add :website, :url, null: false
+      add :featured_posts, # what value for gram posts?
 
       timestamps(type: :utc_datetime_usec)
     end
