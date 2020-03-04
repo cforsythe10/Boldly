de98b631b1f831be071aa379a539352da7858aa0
KedielM <km3556@drexel.edu>
2020-03-03 20:22:09 -0500

    On branch Kediel_DB  Changes to be committed: 	modified:   priv/repo/migrations/20200302220541_create_brands.exs  Rekey Brands

--- backend/elixir/boldly/priv/repo/migrations/20200302220541_create_brands.exs
+++ backend/elixir/boldly/priv/repo/migrations/20200302220541_create_brands.exs
@@ -2,20 +2,20 @@ defmodule Boldly.Repo.Migrations.CreateBrands do
   use Ecto.Migration
 
   def change do
-    create table(:brands, primary_key: false) do
-      add :id, :uuid, null: false, primary_key: true
-      add :ecommerce, :boolean, default: false, null: false
-      add :location, :string
+    create table(:brands) do
+      add :uuid, :uuid, null: false
+      add :ecommerce, :boolean
+      add :location, :string,  null: false
       add :industries, :string
       add :values, :string
       add :email, :string, null: false
-      add :password_hash, :string
+      add :password_hash, :string, null: false
       add :name, :string, null: false
 
       timestamps(type: :utc_datetime_usec)
     end
 
-    create unique_index(:brands, [:id])
+    create unique_index(:brands, [:uuid])
     create unique_index(:brands, [:email])
   end
 end
