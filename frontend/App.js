6fd944abffde5eb01a301f589d5319c7d3bfcc98
Ryan Van Dongen <ryanvdon@gmail.com>
2020-03-06 13:57:58 -0500

    Added custom status bar coloring

--- frontend/App.js
+++ frontend/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux';
 import Navigator from './App/Navigation/Navigator'
 import initStore from './App/Redux/CreateStore';
+import { StatusBar } from 'react-native'
+import { Colors } from './App/Themes'
 
 const store = initStore();
 
@@ -10,6 +12,7 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
+        <StatusBar backgroundColor={Colors.cobalt} translucent={true} barStyle="light-content" />
         <Navigator />
       </Provider>
     )
