452b6cc0a6715a3c017d4b8d649fa16dc312b132
Kevin Tayah <kevin.tayah@nuix.com>
2020-03-09 09:50:06 -0400

    Modified boldly image component

--- frontend/App/Components/Ui/BoldlyImage.js
+++ frontend/App/Components/Ui/BoldlyImage.js
@@ -1,15 +1,29 @@
-import * as React from 'react';
-import { Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Image, View } from 'react-native';
 import PropTypes from 'prop-types';
 import Images from '../../Images/index';
 import LinearGradient from 'react-native-linear-gradient';
 import styles from './Styles/BoldlyImageStyles';
+import axios from 'axios';
 
-const BoldlyImage = ({image, size = 'medium'}) => (
-    <LinearGradient colors={['rgba(0, 0, 0, 0.8)', 'rgba(255, 255, 255, 1)']} style={styles.linearGradient}>
-        <Image source={Images[image]} style={styles[size]} />
-    </LinearGradient>
-);
+const BoldlyImage = ({imageToGet, size = 'medium'}) => {
+    const [imageUri, setImageUri] = useState('');
+
+    const getImage = async image => {
+        const res = await axios.get('https://jsonplaceholder.typicode.com/photos/1'); // placeholder, once I get an idea what the BE endpoint looks like I'll modify this
+        setImageUri(res.data.thumbnailUrl);
+    }
+
+    useEffect(() => {
+        getImage();
+    });
+
+    return imageUri.length && (
+        // <LinearGradient colors={['rgba(0, 0, 0, 0.8)', 'rgba(255, 255, 255, 1)']} style={styles.linearGradient}>
+            <Image source={{uri: imageUri}} style={styles[size]} />
+        // </LinearGradient>
+    );
+};
 
 BoldlyImage.propTypes = {
     image: PropTypes.string.isRequired,
