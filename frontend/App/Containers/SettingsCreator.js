a371eb19c8ae2fad10f18281b6984e94a39923e6
Nataline <natie.kolbe@gmail.com>
2020-03-10 12:01:51 -0400

    everything except the slider toggles

--- frontend/App/Containers/SettingsCreator.js
+++ frontend/App/Containers/SettingsCreator.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, Button, View } from 'react-native';
 
-import Header from '../Components/Ui/Header'
-import BoldlyImage from '../Components/Ui/BoldlyImage'
+import ToggleSwitch from 'toggle-switch-react-native'
+
+import Header from '../Components/Ui/Header';
+import BoldlyImage from '../Components/Ui/BoldlyImage';
+import SecondaryButtonMedium from '../Components/Ui/SecondaryButtonMedium';
+import Edit from '../Images/Icons/edit-icon.svg'
+import Email from '../Images/Icons/email.svg'
+import Password from '../Images/Icons/password.svg'
 
 import styles from './Styles/SettingsScreenStyle';
 
@@ -14,40 +20,90 @@ constructor(props){
 render(){
   return (
     <View style={styles.fullScreen}>
-        <Header headerType='MenuProfile' navigation={this.props.navigation}/>	
-        <View>
-            <Text style={styles.cardHeaderText}>Notifications</Text>
+        <Header headerType='MenuProfile' navigation={this.props.navigation}/>
+
+        <View style={styles.settingsHeader}>
+            <Text style={styles.headerText}>Notifications</Text>
         </View>
-        <View style={styles.centerContentContainer}>
-            <View style={styles.cardContainer}>
-                <View style={styles.cardContentContainer}>
-                    <Text>insights-brand</Text>
-                </View>
+        <View style={styles.settingsCard}>
+            <View style={styles.settingsContent}>
+                <Text>New Matches</Text>
             </View>
         </View>
-        <View style={styles.cardHeader}>
-            <Text style={styles.cardHeaderText}>Account Information</Text>
-        </View>
-        <View style={styles.centerContentContainer}>
-            <View style={styles.cardContainer}>
-                <View style={styles.cardContentContainer}>
-                    <Text>messages-brand</Text>
-                </View>
+        <View style={styles.settingsCard}>
+            <View style={styles.settingsContent}>
+                <Text>New Messages</Text>
             </View>
         </View>
-        <View style={styles.cardHeader}>
-            <Text style={styles.cardHeaderText}>Payment Information</Text>
-            <Text onPress={() => this.props.navigation.navigate('Campaigns')} style={styles.link}>See all</Text>
-        </View>
-        <View style={styles.centerContentContainer}>    
-            <View style={styles.cardContainer}>
-                <View style={styles.cardContentContainer}>
-                    <Text>Campaigns-brand</Text>
-                </View>
+        <View style={styles.settingsCard}>
+            <View style={styles.settingsContent}>
+                <Text>Reminders & News</Text>
             </View>
         </View>
+
+        <View style={styles.settingsHeader}>
+            <Text style={styles.headerText}>Account Information</Text>
+        </View>
+        <View style={styles.settingsCard}>
+            <View style={styles.settingsContent}>
+                <Text><Email height={20} width={20} />   currentemail@gmail.com                             <Edit height={20} width={20} /></Text>
+            </View>
+        </View>
+        <View style={styles.settingsCard}>
+            <View style={styles.settingsContent}>
+                <Text><Password height={20} width={20} />   ********                                                          <Edit height={20} width={20} /></Text>
+            </View>
+        </View>
+
+        <View style={styles.settingsHeader}>
+            <Text style={styles.headerText}>Payment Methods</Text>
+        </View>
+        <View style={styles.settingsCard}>
+            <View style={styles.settingsContent}>
+                <Text>Choose payment method                                     <Edit height={20} width={20} /></Text>
+            </View>
+        </View>
+
+        <View style={styles.settingsHeader}>
+            <Text style={styles.headerText}>Account Status</Text>
+        </View>
+        <View style={styles.settingsCard}>
+            <View style={styles.settingsContent}>
+                <Text>Invisible Mode</Text>
+            </View>
+        </View>
+        <View style={styles.settingsContent}>
+            <Text style={styles.deleteText}>Turn this on to hide your account from new campaigns. All other boldly functions will remain accessible.</Text>
+        </View>
+        
+        <View style={styles.settingsHeader}>
+            <Text style={styles.headerText}>About</Text>
+        </View>
+        <View style={styles.settingsCard}>
+            <View style={styles.settingsContent}>
+                <Text>Privacy policy</Text>
+            </View>
+        </View>
+        <View style={styles.settingsCard}>
+            <View style={styles.settingsContent}>
+                <Text>Terms and conditions</Text>
+            </View>
+        </View>
+        <View style={styles.settingsCard}>
+            <View style={styles.settingsContent}>
+                <Text>Help</Text>
+            </View>
+        </View>
+
+        <View style={styles.deleteSection}>
+            <SecondaryButtonMedium text='Delete account' onPress = {() => navigation.navigate('SettingsCreator')} />
+            <View style={styles.settingsContent}>
+                <Text style={styles.deleteText}>This will remove your profile & settings from our system. This action cannot be undone.</Text>
+            </View>
+        </View>
+
     </View>
-  )	
+  ) 
 }
 
 }
--- frontend/App/Containers/Styles/SettingsScreenStyle.js
+++ frontend/App/Containers/Styles/SettingsScreenStyle.js
@@ -1,14 +1,63 @@
 import { StyleSheet } from 'react-native'
-import { ApplicationStyles, Fonts, Colors } from '../../Themes/'
+import { ApplicationStyles, Fonts, Colors, Metrics } from '../../Themes/'
 
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
+
   headerText:{
-  	...Fonts.style.h3,
+  	fontSize: 16,
+  	fontWeight: 'bold',
   	color: Colors.text
   },
-  // bodyText:{
-  //   ...Fonts.style.body,
-  //   color: Colors.text
-  // },
+
+  bodyText:{
+	...Fonts.style.body,
+	fontSize: 16,
+	color: Colors.text,
+	textAlign: 'left'
+  },
+
+  settingsHeader: {
+    flex: 0.8,
+    marginHorizontal: 15,
+    marginVertical: 10,
+    justifyContent: 'flex-end'
+  },
+
+  settingsCard: {
+    justifyContent: 'center',
+    marginVertical: 5,
+    marginHorizontal: 15,
+    flex: 0.8,
+    paddingVertical: 5,
+    width: '88%',
+    backgroundColor: Colors.white,
+    shadowRadius: 4,
+    borderRadius: 10
+  },
+
+  settingsContent: {
+      justifyContent: 'center',
+      flex: 0.77,
+      paddingHorizontal: '4%',
+  },
+
+  deleteSection: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    alignItems: 'center',
+    marginHorizontal: 15,
+    flex: 2.5,
+    paddingVertical: 3,
+    width: '88%',
+    borderRadius: 10
+  }, 
+
+  deleteText:{
+	...Fonts.style.body,
+	fontSize: 12,
+	color: '#848484',
+	textAlign: 'center',
+	justifyContent: 'flex-end'
+  },
 })
