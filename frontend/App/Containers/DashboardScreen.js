405316439a853177b20a43b38b9cadd4f40d4071
cforsythe555 <40395596+cforsythe555@users.noreply.github.com>
2020-03-10 16:37:03 -0400

    Disambiguation page for settings and small fix to dashboard

--- frontend/App/Containers/DashboardScreen.js
+++ frontend/App/Containers/DashboardScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 import Header from '../Components/Ui/Header'
@@ -11,30 +11,33 @@ import { Fonts, Colors } from '../Themes';
 
 // const DashboardScreen = ({someActionCreator}) => {
 //     someActionCreator('data');
-const DashboardScreen = () => {
-    return (
-    <View style={styles.fullScreen}>
-    	<Header headerType='MenuProfile' navigation={this.props.navigation}/>
-        
-        <View style={{...styles.centerContentContainer, flex: 9}}>
-            <View style={styles.cardContainer}>
-                <View style={styles.cardContentContainer}>
-                    <Text style={{...styles.darkText, textAlign: 'center'}}>Temporary dashboard to show page disambiguation.</Text>
-                    <Text style={{...Fonts.style.h6, paddingTop: '8%'}}>Dashboard for brand not finished profile</Text>
-                    <PrimaryButtonLarge text='Go1' onPress={() => {this.props.navigation.navigate('DashboardMissingProfileB')}} />
-                    <Text style={{...Fonts.style.h6}}>Dashboard for creator not finished profile</Text>
-                    <PrimaryButtonLarge text='Go2' onPress={() => {this.props.navigation.navigate('DashboardMissingProfileC')}} />
-                    <Text style={{...Fonts.style.h6}}>Dashboard for brand with no campaign</Text>
-                    <PrimaryButtonLarge text='Go3' onPress={() => {this.props.navigation.navigate('DashboardNoCampaign')}} />
-                    <Text style={{...Fonts.style.h6}}>Regular dashboard for brand</Text>
-                    <PrimaryButtonLarge text='Go4'  onPress={() => {this.props.navigation.navigate('DashboardRegularB')}} />
-                    <Text style={{...Fonts.style.h6}}>Regular dashboard for creator</Text>
-                    <PrimaryButtonLarge text='Go5' onPress={() => {this.props.navigation.navigate('DashboardRegularC')}} />
-                </View>
-            </View>
-        </View>
-    </View>
-    )
+class DashboardScreen extends Component {
+    render() {
+	    return (
+	    <View style={styles.fullScreen}>
+	    	<Header headerType='MenuProfile' navigation={this.props.navigation}/>
+	        
+	        <View style={{...styles.centerContentContainer, flex: 9}}>
+	            <View style={styles.cardContainer}>
+	                <View style={styles.cardContentContainer}>
+	                    <Text style={{...styles.darkText, textAlign: 'center'}}>Temporary dashboard to show page disambiguation.</Text>
+	                    <Text style={{...Fonts.style.h6, paddingTop: '8%'}}>Dashboard for brand not finished profile</Text>
+	                    <PrimaryButtonLarge key='1' text='Go1' onPress={() => {this.props.navigation.navigate('DashboardMissingProfileB')}} />
+	                    <Text style={{...Fonts.style.h6}}>Dashboard for creator not finished profile</Text>
+	                    <PrimaryButtonLarge text='Go2' key='2' onPress={() => {this.props.navigation.navigate('DashboardMissingProfileC')}} />
+	                    <Text style={{...Fonts.style.h6}}>Dashboard for brand with no campaign</Text>
+	                    <PrimaryButtonLarge text='Go3' key='3' onPress={() => {this.props.navigation.navigate('DashboardNoCampaign')}} />
+	                    <Text style={{...Fonts.style.h6}}>Regular dashboard for brand</Text>
+	                    <PrimaryButtonLarge text='Go4' key='4'  onPress={() => {this.props.navigation.navigate('DashboardRegularB')}} />
+	                    <Text style={{...Fonts.style.h6}}>Regular dashboard for creator</Text>
+	                    <PrimaryButtonLarge text='Go5' key='5' onPress={() => {this.props.navigation.navigate('DashboardRegularC')}} />
+	                    <PrimaryButtonLarge text='Go6' />              
+	                </View>
+	            </View>
+	        </View>
+	    </View>
+	    )
+	}
 }
 
 // const mapStateToProps = state => ({ // Will return all of state to props, don't do this, grab exactly what you need
