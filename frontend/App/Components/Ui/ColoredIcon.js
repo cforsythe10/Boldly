b389b94627217bbbd8c9dfa8ef39de61469193b5
Ryan Van Dongen <ryanvdon@gmail.com>
2020-03-13 08:57:09 -0400

    Removed bug where tapping value icons in the profile crashes the app

--- frontend/App/Components/Ui/ColoredIcon.js
+++ frontend/App/Components/Ui/ColoredIcon.js
@@ -61,10 +61,10 @@ export default class ColoredIcon extends Component {
             
             <View style={styles.valueButtonAlignment}>
                 <TouchableHighlight onPress={() => {
-                    if(!this.props.disabled || this.state.greenButtonState){
-                        this.setState({greenButtonState: !this.state.greenButtonState});
-                        this.props.callback(!this.state.greenButtonState);
-                    }
+                    // if(!this.props.disabled || this.state.greenButtonState){
+                    //     this.setState({greenButtonState: !this.state.greenButtonState});
+                    //     this.props.callback(!this.state.greenButtonState);
+                    // }
                 }} style={this.state.greenButtonState ? styles.greenValueButton : styles.coloredValue} activeOpacity={ 0.5 } underlayColor={ Colors.pear35}> 
                     
                         <View
