a3176085c971e6a05c6d2f2517393cccbb1e6c6f
cforsythe555 <40395596+cforsythe555@users.noreply.github.com>
2020-05-09 18:57:26 -0400

    api function and file picker for messaging

--- frontend/App/Containers/DirectMessages.js
+++ frontend/App/Containers/DirectMessages.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { View, ScrollView, Text, TextInput, Image, TouchableOpacity } from 'react-native';
+import DocumentPicker from 'react-native-document-picker';
+import * as RNFS from 'react-native-fs';
 
 import Header from '../Components/Ui/Header';
 import MessageBox from '../Components/Ui/MessageBox';
+import { makePost, uploadFile } from '../Services/Api.js';
 
 import Close from '../Images/Icons/close.svg';
 import Import from '../Images/Icons/import.svg';
@@ -25,6 +28,29 @@ export default class DirectMessages extends Component {
 		this.setState({ messages: [...this.state.messages, { text: this.state.currentMessage, fromUser: true }], currentMessage: '' });
 	}
 
+	async uploadClicked() {
+		try {
+		  	const res = await DocumentPicker.pick({
+		    	type: ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'],
+		 	});
+
+			const split = res.uri.split('/');
+			const name = split.pop();
+			const inbox = split.pop();
+			const realPath = `${RNFS.TemporaryDirectoryPath}${inbox}/${name}`;
+
+			console.log(realPath);
+
+			uploadFile(res, 'fill with endpoint', realPath);
+		} catch (err) {
+		  if (DocumentPicker.isCancel(err)) {
+		    // User cancelled the picker, exit any dialogs or menus and move on
+		  } else {
+		    throw err;
+		  }
+		}
+	}
+
 	renderSend() {
 		return(
 			<TouchableOpacity onPress={() => this.sendText()}>
@@ -43,14 +69,11 @@ export default class DirectMessages extends Component {
 
 	renderOptions() {
 		return(
-			<View style={ styles.optionsContainer }>
+			<TouchableOpacity onPress={() => this.uploadClicked()}>
 				<View style={ styles.optionWrapper }>
 					<Import height={12} width={15} stroke={ Colors.fog } />
 				</View>
-				<TouchableOpacity style={ styles.optionWrapper } onPress={() => this.setState({ optionsOpened: false })}>
-					<Close height={10} width={10} stroke={ Colors.fog } />
-				</TouchableOpacity>
-			</View>
+			</TouchableOpacity>
 		)
 	}
 
@@ -65,8 +88,7 @@ export default class DirectMessages extends Component {
 					onSubmitEditing={() => this.sendText()}
 					value={ this.state.currentMessage }
 				/>
-				{this.state.currentMessage === '' && this.state.optionsOpened ? this.renderOptions() : null }
-				{this.state.currentMessage === '' && !this.state.optionsOpened ? this.renderPlusOption() : null }
+				{this.state.currentMessage === '' ? this.renderOptions() : null}
 				{this.state.currentMessage !== '' ? this.renderSend() : null }
 			</View>
 		)
--- frontend/App/Services/Api.js
+++ frontend/App/Services/Api.js
@@ -1,5 +1,7 @@
-let url = 'http://127.0.0.1:4000/';
+import RNFetchBlob from 'react-native-fetch-blob';
+import * as RNFS from 'react-native-fs';
 
+let url = 'http://10.0.2.2:4000/';
 
 
 export function makePost(endpoint, body){
@@ -20,4 +22,48 @@ export function makeGet(endpoint, body){
     method: 'GET',
     //finish this if needed 
   })
+}
+
+export function uploadFile(endpoint, file, path){
+  let thisUrl = url + endpoint;
+
+  var body = new FormData();
+  body.append('file', file);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', thisUrl);
+  xhr.send(body);
+
+/*
+  const data = new FormData();
+
+  data.append('file', {
+    name: file.fileName,
+    type: file.type,
+    uri:
+      Platform.OS === 'android' ? file.uri : file.uri.replace('file://', ''),
+  });
+
+  console.log(data);
+
+  Object.keys(data).forEach(key => {
+    data.append(key, data[key]);
+  });
+
+  console.log(data);
+
+  return fetch(thisUrl, {
+    method:'POST',
+    headers: {
+      'Content-Type': 'multipart/formdata'
+    },
+    body: data,
+  })
+    .then(response => response.json())
+    .then(response => {
+      console.log('success', response);
+    })
+    .catch(error => {
+      console.log('error: ', error);
+    })*/
 }
