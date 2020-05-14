0b2dadcd60465b9427b11483e1342ce7c66116c4
Samuel Platek <moneymop@gmail.com>
2020-05-14 19:24:38 -0400

    docs for messaging and conversations are complete
    Signed-off-by: Samuel Platek <moneymop@gmail.com>

--- backend/elixir/boldly/lib/boldly_web/views/message_view.ex
+++ backend/elixir/boldly/lib/boldly_web/views/message_view.ex
@@ -2,6 +2,17 @@ defmodule BoldlyWeb.MessageView do
   use BoldlyWeb, :view
   alias BoldlyWeb.MessageView
 
+  @doc """
+  Returns a JSON object which is either a list or a single item which is composed of the following fields:
+
+  Attribute fields (returned under a `data` field) are:
+
+  conversation_id,
+  id,
+  content,
+  sent_by_creator,
+  date
+  """
   def render("index.json", %{messages: messages}) do
     %{data: render_many(messages, MessageView, "message.json")}
   end
