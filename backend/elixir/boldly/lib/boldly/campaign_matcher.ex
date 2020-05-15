da234ac1576b9848539b26a3e9bc7c87399e4527
Samuel Platek <moneymop@gmail.com>
2020-05-15 14:44:19 -0400

    setting up some property testing for campaign matches
    Signed-off-by: Samuel Platek <moneymop@gmail.com>

--- backend/elixir/boldly/lib/boldly/campaign_matcher.ex
+++ backend/elixir/boldly/lib/boldly/campaign_matcher.ex
@@ -15,22 +15,16 @@ defmodule Boldly.CampaignMatcher do
 
     campaign
     |> compose_query()
-    |> get_scores
-
-  end
-
-  def comp_q(vals \\ [1,2,3], interests \\ [4,5,6]) do
-    val_query = vals_query(vals)
-    interests_query = int_query(from(c in subquery(val_query)), interests)
+    |> Repo.all()
   end
 
   def compose_query(camp) do
     {start_date, end_date} = get_age_range(camp.age_range)
     industry = camp.industry
     vals = String.split(camp.values, ",")
-    interests = camp.interests
+    interests = String.split(camp.interests, ",")
 
-    val_query = vals_query(vals)
+    val_query = vals_query(Creator, vals)
     interests_query = int_query(from(c in subquery(val_query)), interests)
 
     # date_query = where([c], c.birthday >= ^start_date and c.birthday <= ^end_date)
@@ -39,16 +33,29 @@ defmodule Boldly.CampaignMatcher do
 
     q_base
     |> where([c], c.industry == ^industry)
-    |> where([c], c.birthday >= ^start_date and c.birthday <= ^end_date)
+    |> where([c], c.birthday <= ^start_date and c.birthday >= ^end_date)
+    |> location_query(camp.location, camp.specific_to_location)
   end
 
-  def get_age_range(age_range) do
+  def get_age_range(age_range \\ "18-50") do
     [s, e] = String.split(age_range, "-")
     s_i = String.to_integer(s)
     e_i = String.to_integer(e)
 
-    
+    start_date = Date.add(Date.utc_today(), (-365) * s_i)
+    end_date = Date.add(Date.utc_today(), (-365) * e_i)
+
+    {start_date, end_date}
   end
+
+  def location_query(query, loc, false), do: query
+
+  def location_query(query, loc, true) do
+    query
+    |> where([c], c.location == ^loc)
+  end
+
+  def int_query(query, [""]), do: query
 
   def int_query(query, interests) do
     Enum.reduce(interests, query, fn int, qt ->
@@ -59,9 +66,9 @@ defmodule Boldly.CampaignMatcher do
 
 
 
-  # def vals_query(query, nil), do: query
+  def vals_query(query, [""]), do: query
 
-  def vals_query(val_list, query \\ from(c in Creator)) do
+  def vals_query(query, val_list) do
     Enum.reduce(val_list, query, fn val, qt ->
       q_t = "%#{val}%"
       from m in qt, or_where: like(m.values, ^q_t)
---
+++ backend/elixir/boldly/test/boldly/campaign_matcher_test.exs
@@ -0,0 +1,152 @@
+defmodule Boldly.CampaignMatcherTest do
+  use Boldly.DataCase
+
+
+  alias Boldly.CampaignInfo.Campaign
+  alias Boldly.CreatorAccount.Creator
+
+  alias Boldly.CampaignInfo
+  alias Boldly.CreatorAccount
+  alias Boldly.CampaignMatcher
+
+  @valid_creator_attrs1 %{
+      birthday: ~D[1990-04-17],
+      email: "some email",
+      industry: "some industry",
+      interests: "some interests",
+      location: "some location",
+      name: "some name",
+      values: "some values",
+      password: "some password"
+    }
+
+    @valid_creator_attrs2 %{
+      birthday: ~D[2012-04-17],
+      email: "some email2",
+      industry: "some industry2",
+      interests: "some interests2",
+      location: "some location2",
+      name: "some name2",
+      values: "some values2",
+      password: "some password2"
+    }
+
+    @valid_brand_attrs %{
+        ecommerce: true,
+        email: "some email",
+        uuid: "7488a646-e31f-11e4-aace-600308960660",
+        id: 1,
+        industries: "some industries",
+        location: "some location",
+        values: "some values",
+        password: "some password",
+        name: "some name"
+      }
+
+      @valid_campaign_attrs %{
+        age_range: "18-35",
+        compensation: "some compensation",
+        creator_responsibilities: "some creator_responsibilities",
+        description: "some description",
+        desired_engagement_rate: 42,
+        end_date: ~D[2010-04-17],
+        industry: "some industry",
+        interests: "some interests",
+        is_draft: false,
+        location: "some location",
+        name: "some name",
+        perks: "some perks",
+        photo_reference: "some photo_reference",
+        specific_to_location: true,
+        start_date: ~D[2010-04-17],
+        uuid: "7488a646-e31f-11e4-aace-600308960662",
+        values: "some values",
+        launched_by: "7488a646-e31f-11e4-aace-600308960660"
+      }
+
+    def campaign_fixture(attrs \\ %{}) do
+      {:ok, brand} = @valid_brand_attrs |> Boldly.BrandAccount.create_brand()
+      {:ok, creator1} = @valid_creator_attrs1 |> Boldly.CreatorAccount.create_creator()
+
+      {:ok, creator2} = @valid_creator_attrs2 |> Boldly.CreatorAccount.create_creator()
+
+      {:ok, campaign} = attrs |> Enum.into(Map.put(@valid_campaign_attrs, :launched_by, brand.uuid)) |> CampaignInfo.create_campaign()
+
+      {campaign, creator1, creator2}
+    end
+
+
+
+  # attrs ={
+  #   date_range: Date.range(~D[1999-01-01], ~D[2000-01-01]),
+  #   industry: "industry",
+  #   vals: list of vals -- will combine into string if multiple are chose,
+  #   max_num_vals: should not exceed number of elements in vals
+  #   interests: list like vals,
+  #   max_num_int: should not exceed number of interests,
+  #  location: "location"
+  #   }
+    def create_users_with_attributes(attrs, num_create \\ 5) do
+      email_stream = StreamData.string(:alphanumeric, min_length: 20, max_length: 50)
+      Enum.map(1..num_create, fn _ ->
+        [d] =  Enum.take(Enum.shuffle(attrs.date_range), 1)
+        [email] = Enum.take(email_stream, 1)
+        [n_vals] = Enum.take(StreamData.integer(1..attrs.max_num_vals), 1)
+        [n_ints] = Enum.take(StreamData.integer(1..attrs.max_num_int), 1)
+        vals = Enum.take(StreamData.member_of(attrs.vals), n_vals)
+        ints = Enum.take(StreamData.member_of(attrs.interests), n_ints)
+        loc = attrs.location
+
+        values =
+        if Enum.count(vals) == 1 do
+          Enum.at(vals, 0)
+        else
+          Enum.reduce(vals, "", fn v, str ->
+            if str == "" do
+              str = v
+            else
+              str <> ",#{v}"
+            end
+          end)
+        end
+
+        interests =
+          if Enum.count(ints) == 1 do
+            Enum.at(ints, 0)
+          else
+            Enum.reduce(ints, "", fn i, str ->
+              if str == "" do
+                str = i
+              else
+                str <> ",#{i}"
+              end
+
+            end)
+          end
+
+        {:ok, creator} =
+          %{birthday: d, email: email, values: values, interests: interests, location: loc}
+          |> Enum.into(@valid_creator_attrs1)
+          |> CreatorAccount.create_creator()
+
+
+        creator
+
+      end)
+
+    end
+
+
+    describe "main algorithm" do
+      test "main algorithm returns correct applicant" do
+        {camp, creator1, creator2} = campaign_fixture()
+
+        res = CampaignMatcher.match(camp.id)
+
+        c1 = Boldly.CreatorAccount.get_creator!(creator1.id)
+        # IO.puts(res)
+
+        assert res == [c1]
+      end
+    end
+end
